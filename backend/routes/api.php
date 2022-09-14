d7f8b9650300b6f9e7954d522e34a919881bed6c
George Bien A. Almenanza <gbienalmenanza@gmail.com>
2022-09-14 12:32:02 +0800

    Recreated backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,28 +1,19 @@
 <?php
 
-use App\Http\Controllers\WeatherController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get(
-    '/get-cities',
-    [
-        WeatherController::class, 
-        'getCities'
-    ]
-);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-Route::get(
-    '/get-weather',
-    [
-        WeatherController::class, 
-        'getWeather'
-    ]
-);
-
-Route::get(
-    '/get-forecast',
-    [
-        WeatherController::class, 
-        'getForecast'
-    ]
-);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
